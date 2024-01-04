d4e3f05de851ab2aaaca96682d99ef750cbe4e8d
Caleb Sword <85499281+calebmsword@users.noreply.github.com>
2024-01-04 16:20:03 -0500

    Create public-types.d.ts

---
+++ public-types.d.ts
@@ -0,0 +1,10 @@
+// Any types written in this file can be imported in your NPM package.
+// Don't include types used for implementation details not exposed to the user. 
+// Create separate type files for implementation details.
+
+// If you want your NPM package to have types, include those here. See
+// https://github.com/calebmsword/clone-deep/blob/main/public-types.d.ts
+// for an example.
+declare module "npm-package-name" {
+
+}
