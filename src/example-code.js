c18d7ae62f64a6556bf003ba9df09bf179f21af7
Caleb Sword <85499281+calebmsword@users.noreply.github.com>
2024-01-04 16:40:05 -0500

    Create example-code.js

---
+++ src/example-code.js
@@ -0,0 +1,26 @@
+// Include all javascript in the src files.
+
+// Write /** @type {Type} */ to get syntax highlighting
+
+// Use /** @type {import("./relative/path/to/.d.ts").Type} */ to import types
+
+// Use the following syntax to document functions:
+
+/**
+ * A short description of the function.
+ * A longer description of the function can follow.
+ * {@link variableName} creates a link to variableName.
+ * For example code:
+ * @example
+ * ```javascript
+ * // javascript code
+ * ```
+ * You can provide multiple examples.
+ * @param {TypeA} paramName A description of the parameter
+ * @param {TypeB} hashParam Provide a type for an object parameter
+ * @param {object} hashParam An object parameter. You MUST use the `object` type for the properties to be documented
+ * @param {TypeC} hashParam.property Descriptions of a property on the hashParam object
+ * @param {TypeD} [optionalParam] The brackets indicate the argument is optional
+ * @returns {TypeD}
+ */
+// const myFunction = (paramName, { hashParam }, optionalParam) => { /** do stuff */ };
