ab91ad35a15b9718dcab15d1d6337f4b90acc1bf
Caleb Sword <85499281+calebmsword@users.noreply.github.com>
2024-01-04 17:31:03 -0500

    Create example-code.test.js

---
+++ src/example-code.test.js
@@ -0,0 +1,20 @@
+import assert from "node:assert";
+import { describe, test, mock } from "node:test";
+
+// By default, TypeScript will not check test files.
+// If you would like TypeScript in these files, then
+// in tsconfig.json:
+//  - remove `"exclude"`
+//  - add `"moduleResolution": "nodeNext"`
+//  - add `"allowSyntheticDefaultImports": true`
+
+describe("logical group of tests", () => {
+    // You might find it convenient to logically group tests under describe blocks.
+    // This is not necessary. You can only use `test` if you want.
+    test("description of test", () => {
+        assert.strictEqual(true  /* expected */, true /* actual */);
+    });
+
+    // see https://nodejs.org/api/test.html#mocking
+    // for examples of the mock API
+});
